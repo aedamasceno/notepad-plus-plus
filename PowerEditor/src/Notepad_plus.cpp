aedamasceno/notepad-plus-plus

commit 073f0bc8c6333201f057a177606b1d4c90b5fe2f

Enable auto-Indent for PowerShell
Fix #9049, close #9051

PowerEditor/src/Notepad_plus.cpp
@@ -2953,7 +2953,7 @@ void Notepad_plus::maintainIndentation(TCHAR ch)
 		return;
 
 	if (type == L_C || type == L_CPP || type == L_JAVA || type == L_CS || type == L_OBJC ||
-		type == L_PHP || type == L_JS || type == L_JAVASCRIPT || type == L_JSP || type == L_CSS || type == L_PERL || type == L_RUST)
+		type == L_PHP || type == L_JS || type == L_JAVASCRIPT || type == L_JSP || type == L_CSS || type == L_PERL || type == L_RUST || type == L_POWERSHELL)
 	{
 		if (((eolMode == SC_EOL_CRLF || eolMode == SC_EOL_LF) && ch == '\n') ||
 			(eolMode == SC_EOL_CR && ch == '\r'))
@@ -2996,7 +2996,7 @@ void Notepad_plus::maintainIndentation(TCHAR ch)
 				_pEditView->setLineIndent(curLine, indentAmountPrevLine);
 			}
 			// These languages do no support single line control structures without braces.
-			else if (type == L_PERL || type == L_RUST)
+			else if (type == L_PERL || type == L_RUST || L_POWERSHELL)
 			{
 				_pEditView->setLineIndent(curLine, indentAmountPrevLine);
 			}

commit b7d199f811913dbde0058faa1d1313072535b58a

Fix regression of auto-Indent
The regression is introduced by https://github.com/notepad-plus-plus/notepad-plus-plus/commit/073f0bc8c6333201f057a177606b1d4c90b5fe2f

Fix #9113, close #9114

PowerEditor/src/Notepad_plus.cpp
@@ -3054,7 +3054,7 @@ void Notepad_plus::maintainIndentation(TCHAR ch)
 				_pEditView->setLineIndent(curLine, indentAmountPrevLine);
 			}
 			// These languages do no support single line control structures without braces.
-			else if (type == L_PERL || type == L_RUST || L_POWERSHELL)
+			else if (type == L_PERL || type == L_RUST || type == L_POWERSHELL)
 			{
 				_pEditView->setLineIndent(curLine, indentAmountPrevLine);
 			}