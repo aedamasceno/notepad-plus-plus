aedamasceno/notepad-plus-plus

commit c0be50494ceba80bd1c380f64c9c02391f926526

Fix NPPM_RELOADFILE with 8.3 path containing bug
Fixes issue seen with pythonscript testcase https://github.com/bruderstein/PythonScript/blob/master/PythonScript/python_tests/tests/NotepadWrapperTestCase.py#L383-L397 using NPPM_RELOADFILE with a 8.3 path containing.

Fix #11106, close #11107

PowerEditor/src/ScintillaComponent/Buffer.cpp
@@ -1599,7 +1599,7 @@ BufferID FileManager::getBufferFromName(const TCHAR* name)
 
 	for (size_t i = 0; i < _buffers.size(); i++)
 	{
-		if (OrdinalIgnoreCaseCompareStrings(name, _buffers.at(i)->getFullPathName()) == 0)
+		if (OrdinalIgnoreCaseCompareStrings(fullpath, _buffers.at(i)->getFullPathName()) == 0)
 		{
 			if (_buffers.at(i)->_referees[0]->isVisible())
 			{

commit 30961405ee58cad28d4bd0f572fbbbbadd02720b

Fix incorrect message on double click on search result regression
This regression was inserted by commit c0be50494ceba80bd1c380f64c9c02391f926526 (PR #11107).

Fix #11215, Fix #11106

PowerEditor/src/NppBigSwitch.cpp
@@ -505,7 +505,15 @@ LRESULT Notepad_plus::process(HWND hwnd, UINT message, WPARAM wParam, LPARAM lPa
 
 		case NPPM_RELOADFILE:
 		{
-			BufferID id = MainFileManager.getBufferFromName(reinterpret_cast<const TCHAR *>(lParam));
+			TCHAR longNameFullpath[MAX_PATH];
+			const TCHAR* pFilePath = reinterpret_cast<const TCHAR*>(lParam);
+			wcscpy_s(longNameFullpath, MAX_PATH, pFilePath);
+			if (_tcschr(longNameFullpath, '~'))
+			{
+				::GetLongPathName(longNameFullpath, longNameFullpath, MAX_PATH);
+			}
+
+			BufferID id = MainFileManager.getBufferFromName(longNameFullpath);
 			if (id != BUFFER_INVALID)
 				doReload(id, wParam != 0);
 			break;

PowerEditor/src/ScintillaComponent/Buffer.cpp
@@ -1621,20 +1621,13 @@ bool FileManager::loadFileData(Document doc, int64_t fileSize, const TCHAR * fil
 
 BufferID FileManager::getBufferFromName(const TCHAR* name)
 {
-	TCHAR fullpath[MAX_PATH];
-	::GetFullPathName(name, MAX_PATH, fullpath, NULL);
-	if (_tcschr(fullpath, '~'))
+	for (auto buf : _buffers)
 	{
-		::GetLongPathName(fullpath, fullpath, MAX_PATH);
-	}
-
-	for (size_t i = 0; i < _buffers.size(); i++)
-	{
-		if (OrdinalIgnoreCaseCompareStrings(fullpath, _buffers.at(i)->getFullPathName()) == 0)
+		if (OrdinalIgnoreCaseCompareStrings(name, buf->getFullPathName()) == 0)
 		{
-			if (_buffers.at(i)->_referees[0]->isVisible())
+			if (buf->_referees[0]->isVisible())
 			{
-				return _buffers.at(i)->getID();
+				return buf->getID();
 			}
 		}
 	}