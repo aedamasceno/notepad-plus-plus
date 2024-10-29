aedamasceno/notepad-plus-plus

commit f884a39dd422cda908b36e9439fb00c340c4d85e

Fix opened network files hanging issue while the network disconnected
The issue is due to WinAPI's GetFileAttributes function can take a huge amount of time in various situations, like when the target server being offline (usalally).
The current solution is to wait at least 3 seconds for each GetFileAttributes function, then return the default value in case of the problem (or the returned value of the function, in case of normal situation - if it has been executed completely during the 3 seconds).
So there'll still be the hanging time (3 or 6 seconds) while the problem of network connection, but the hanging time (it could be 30 seconds more) is reduced considerablly.

"Wow64EnableWow64FsRedirection" call is also removed from x64 build (in which this call is unnecessary) in this commit to reduce the IO calls.

Fix #4306, fix #6178, fix #8055, fix #11388, fix #12553, fix #15540, close #15658

PowerEditor/src/WinControls/OpenSaveFileDialog/CustomFileDialog.cpp
@@ -16,7 +16,6 @@
 
 
 #include <shobjidl.h>
-#include <shlwapi.h>	// PathIsDirectory
 #ifdef __MINGW32__
 #include <cwchar>
 #endif
@@ -142,7 +141,7 @@ namespace // anonymous
 		HRESULT hr = SHCreateItemFromParsingName(path,
 			nullptr,
 			IID_PPV_ARGS(&shellItem));
-		if (SUCCEEDED(hr) && shellItem && !::PathIsDirectory(path))
+		if (SUCCEEDED(hr) && shellItem && !::doesDirectoryExist(path))
 		{
 			com_ptr<IShellItem> parentItem;
 			hr = shellItem->GetParent(&parentItem);
@@ -506,7 +505,7 @@ private:
 		expandEnv(fileName);
 		bool nameChanged = transformPath(fileName);
 		// Update the controls.
-		if (!::PathIsDirectory(getAbsPath(fileName).c_str()))
+		if (doesDirectoryExist(getAbsPath(fileName).c_str()))
 		{
 			// Name is a file path.
 			// Add file extension if missing.

PowerEditor/src/MISC/Common/Common.cpp
@@ -1213,7 +1213,7 @@ bool isCertificateValidated(const wstring & fullFilePath, const wstring & subjec
 
 bool isAssoCommandExisting(LPCTSTR FullPathName)
 {
-	bool isAssoCommandExisting = false;
+	bool isAssoCmdExist = false;
 
 	bool isFileExisting = doesFileExist(FullPathName);
 
@@ -1228,11 +1228,11 @@ bool isAssoCommandExisting(LPCTSTR FullPathName)
 		// check if association exist
 		hres = AssocQueryString(ASSOCF_VERIFY|ASSOCF_INIT_IGNOREUNKNOWN, ASSOCSTR_COMMAND, ext, NULL, buffer, &bufferLen);
 
-        isAssoCommandExisting = (hres == S_OK)                  // check if association exist and no error
-			&& (wcsstr(buffer, L"notepad++.exe")) == NULL; // check association with notepad++
+		isAssoCmdExist = (hres == S_OK)                  // check if association exist and no error
+			&& (wcsstr(buffer, L"notepad++.exe")) == NULL;   // check association with notepad++
 
 	}
-	return isAssoCommandExisting;
+	return isAssoCmdExist;
 }
 
 std::wstring s2ws(const std::string& str)
@@ -1764,20 +1764,183 @@ bool Version::isCompatibleTo(const Version& from, const Version& to) const
 	return false;
 }
 
-bool doesFileExist(const wchar_t* filePath)
+//----------------------------------------------------
+struct GetAttrParamResult {
+	std::wstring _filePath;
+	DWORD _fileAttr = INVALID_FILE_ATTRIBUTES;
+	bool _isNetworkFailure = true;
+};
+
+DWORD WINAPI getFileAttributesWorker(void* data)
 {
-	DWORD dwAttrib = ::GetFileAttributesW(filePath);
-	return (dwAttrib != INVALID_FILE_ATTRIBUTES && !(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+	GetAttrParamResult* inAndOut = static_cast<GetAttrParamResult*>(data);
+	inAndOut->_fileAttr = ::GetFileAttributesW(inAndOut->_filePath.c_str());
+	inAndOut->_isNetworkFailure = false;
+	return TRUE;
+};
+
+DWORD getFileAttrWaitSec(const wchar_t* filePath, DWORD milleSec2wait, bool* isNetWorkProblem)
+{
+	GetAttrParamResult data;
+	data._fileAttr = INVALID_FILE_ATTRIBUTES;
+	data._filePath = filePath;
+	data._isNetworkFailure = true;
+
+	HANDLE hThread = ::CreateThread(NULL, 0, getFileAttributesWorker, &data, 0, NULL);
+	if (!hThread)
+	{
+		return INVALID_FILE_ATTRIBUTES;
+	}
+
+	// wait for our worker thread to complete or terminate it when the required timeout has elapsed
+	DWORD dwWaitStatus = ::WaitForSingleObject(hThread, milleSec2wait == 0 ? 1000 : milleSec2wait);
+	switch (dwWaitStatus)
+	{
+		case WAIT_OBJECT_0: // Ok, the state of our worker thread is signaled, so it finished itself in the timeout given		
+			// - nothing else to do here, except the thread handle closing later
+			break;
+
+		case WAIT_TIMEOUT: // the timeout interval elapsed, but the worker's state is still non-signaled
+		default: // any other dwWaitStatus is a BAD one here
+			// WAIT_FAILED or WAIT_ABANDONED
+			::TerminateThread(hThread, dwWaitStatus);
+			break;
+	}
+	CloseHandle(hThread);
+	
+	if (isNetWorkProblem != nullptr)
+		*isNetWorkProblem = data._isNetworkFailure;
+
+	return data._fileAttr;
+};
+
+bool doesFileExist(const wchar_t* filePath, DWORD milleSec2wait, bool* isNetWorkProblem)
+{
+	DWORD attr = getFileAttrWaitSec(filePath, milleSec2wait, isNetWorkProblem);
+	return (attr != INVALID_FILE_ATTRIBUTES && !(attr & FILE_ATTRIBUTE_DIRECTORY));
 }
 
-bool doesDirectoryExist(const wchar_t* dirPath)
+bool doesDirectoryExist(const wchar_t* dirPath, DWORD milleSec2wait, bool* isNetWorkProblem)
 {
-	DWORD dwAttrib = ::GetFileAttributesW(dirPath);
-	return (dwAttrib != INVALID_FILE_ATTRIBUTES && (dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+	DWORD attr = getFileAttrWaitSec(dirPath, milleSec2wait, isNetWorkProblem);
+	return (attr != INVALID_FILE_ATTRIBUTES && (attr & FILE_ATTRIBUTE_DIRECTORY));
 }
 
-bool doesPathExist(const wchar_t* path)
+bool doesPathExist(const wchar_t* path, DWORD milleSec2wait, bool* isNetWorkProblem)
 {
-	DWORD dwAttrib = ::GetFileAttributesW(path);
-	return (dwAttrib != INVALID_FILE_ATTRIBUTES);
+	DWORD attr = getFileAttrWaitSec(path, milleSec2wait, isNetWorkProblem);
+	return (attr != INVALID_FILE_ATTRIBUTES);
+}
+
+//----------------------------------------------------
+
+struct GetDiskFreeSpaceParamResult
+{
+	std::wstring _dirPath;
+	ULARGE_INTEGER _freeBytesForUser {};
+	DWORD _result = FALSE;
+	bool _isNetworkFailure = true;
+};
+
+DWORD WINAPI getDiskFreeSpaceExWorker(void* data)
+{
+	GetDiskFreeSpaceParamResult* inAndOut = static_cast<GetDiskFreeSpaceParamResult*>(data);
+	inAndOut->_result = ::GetDiskFreeSpaceExW(inAndOut->_dirPath.c_str(), &(inAndOut->_freeBytesForUser), nullptr, nullptr);
+	inAndOut->_isNetworkFailure = false;
+	return TRUE;
+};
+
+DWORD getDiskFreeSpaceWaitSec(const wchar_t* dirPath, ULARGE_INTEGER* freeBytesForUser, DWORD milleSec2wait, bool* isNetWorkProblem)
+{
+	GetDiskFreeSpaceParamResult data;
+	data._dirPath = dirPath;
+	data._freeBytesForUser = {};
+	data._result = FALSE;
+	data._isNetworkFailure = true;
+
+	HANDLE hThread = ::CreateThread(NULL, 0, getDiskFreeSpaceExWorker, &data, 0, NULL);
+	if (!hThread)
+	{
+		return FALSE;
+	}
+
+	// wait for our worker thread to complete or terminate it when the required timeout has elapsed
+	DWORD dwWaitStatus = ::WaitForSingleObject(hThread, milleSec2wait == 0 ? 1000 : milleSec2wait);
+	switch (dwWaitStatus)
+	{
+		case WAIT_OBJECT_0: // Ok, the state of our worker thread is signaled, so it finished itself in the timeout given		
+			// - nothing else to do here, except the thread handle closing later
+			break;
+
+		case WAIT_TIMEOUT: // the timeout interval elapsed, but the worker's state is still non-signaled
+		default: // any other dwWaitStatus is a BAD one here
+			// WAIT_FAILED or WAIT_ABANDONED
+			::TerminateThread(hThread, dwWaitStatus);
+			break;
+	}
+	CloseHandle(hThread);
+
+	*freeBytesForUser = data._freeBytesForUser;
+
+	if (isNetWorkProblem != nullptr)
+		*isNetWorkProblem = data._isNetworkFailure;
+
+	return data._result;
+}
+
+
+//----------------------------------------------------
+
+struct GetAttrExParamResult
+{
+	std::wstring _filePath;
+	WIN32_FILE_ATTRIBUTE_DATA _attributes{};
+	DWORD _result = FALSE;
+	bool _isNetworkFailure = true;
+};
+
+DWORD WINAPI getFileAttributesExWorker(void* data)
+{
+	GetAttrExParamResult* inAndOut = static_cast<GetAttrExParamResult*>(data);
+	inAndOut->_result = ::GetFileAttributesEx(inAndOut->_filePath.c_str(), GetFileExInfoStandard, &(inAndOut->_attributes));
+	inAndOut->_isNetworkFailure = false;
+	return TRUE;
+};
+
+DWORD getFileAttributesExWaitSec(const wchar_t* filePath, WIN32_FILE_ATTRIBUTE_DATA* fileAttr, DWORD milleSec2wait, bool* isNetWorkProblem)
+{
+	GetAttrExParamResult data;
+	data._filePath = filePath;
+	data._attributes = {};
+	data._result = FALSE;
+	data._isNetworkFailure = true;
+
+	HANDLE hThread = ::CreateThread(NULL, 0, getFileAttributesExWorker, &data, 0, NULL);
+	if (!hThread)
+	{
+		return FALSE;
+	}
+
+	// wait for our worker thread to complete or terminate it when the required timeout has elapsed
+	DWORD dwWaitStatus = ::WaitForSingleObject(hThread, milleSec2wait == 0 ? 1000 : milleSec2wait);
+	switch (dwWaitStatus)
+	{
+		case WAIT_OBJECT_0: // Ok, the state of our worker thread is signaled, so it finished itself in the timeout given		
+			// - nothing else to do here, except the thread handle closing later
+			break;
+
+		case WAIT_TIMEOUT: // the timeout interval elapsed, but the worker's state is still non-signaled
+		default: // any other dwWaitStatus is a BAD one here
+			// WAIT_FAILED or WAIT_ABANDONED
+			::TerminateThread(hThread, dwWaitStatus);
+			break;
+	}
+	CloseHandle(hThread);
+
+	*fileAttr = data._attributes;
+
+	if (isNetWorkProblem != nullptr)
+		*isNetWorkProblem = data._isNetworkFailure;
+
+	return data._result;
 }

PowerEditor/src/MISC/Common/Common.h
@@ -25,7 +25,8 @@
 #include <algorithm>
 #include <tchar.h>
 
-#pragma deprecated(PathFileExists) // Use doesFileExist, doesDirectoryExist or doesPathExist (for file or directory) instead.
+#pragma deprecated(PathFileExists)  // Use doesFileExist, doesDirectoryExist or doesPathExist (for file or directory) instead.
+#pragma deprecated(PathIsDirectory) // Use doesDirectoryExist instead.
 
 
 const bool dirUp = true;
@@ -280,6 +281,9 @@ private:
 	unsigned long _build = 0;
 };
 
-bool doesFileExist(const wchar_t* filePath);
-bool doesDirectoryExist(const wchar_t* dirPath);
-bool doesPathExist(const wchar_t* path);
+bool doesFileExist(const wchar_t* filePath, DWORD milleSec2wait = 0, bool* isNetWorkProblem = nullptr);
+bool doesDirectoryExist(const wchar_t* dirPath, DWORD milleSec2wait = 0, bool* isNetWorkProblem = nullptr);
+bool doesPathExist(const wchar_t* path, DWORD milleSec2wait = 0, bool* isNetWorkProblem = nullptr);
+
+DWORD getDiskFreeSpaceWaitSec(const wchar_t* dirPath, ULARGE_INTEGER* freeBytesForUser, DWORD milleSec2wait = 0, bool* isNetWorkProblem = nullptr);
+DWORD getFileAttributesExWaitSec(const wchar_t* filePath, WIN32_FILE_ATTRIBUTE_DATA* fileAttr, DWORD milleSec2wait = 0, bool* isNetWorkProblem = nullptr);

PowerEditor/src/Notepad_plus.cpp
@@ -2579,7 +2579,8 @@ void Notepad_plus::checkDocState()
 
 	bool isCurrentDirty = curBuf->isDirty();
 	bool isSeveralDirty = isCurrentDirty;
-	bool isFileExisting = doesFileExist(curBuf->getFullPathName());
+	bool isNetworkProblem;
+	bool isFileExisting = doesFileExist(curBuf->getFullPathName(), 1000, &isNetworkProblem);
 	if (!isCurrentDirty)
 	{
 		for (size_t i = 0; i < MainFileManager.getNbBuffers(); ++i)
@@ -2613,7 +2614,7 @@ void Notepad_plus::checkDocState()
 	enableCommand(IDM_FILE_RELOAD, isFileExisting, MENU);
 	enableCommand(IDM_FILE_CONTAININGFOLDERASWORKSPACE, isFileExisting, MENU);
 
-	enableCommand(IDM_FILE_OPEN_DEFAULT_VIEWER, isAssoCommandExisting(curBuf->getFullPathName()), MENU);
+	enableCommand(IDM_FILE_OPEN_DEFAULT_VIEWER, isFileExisting ? isAssoCommandExisting(curBuf->getFullPathName()) : false, MENU);
 
 	enableCommand(IDM_VIEW_IN_FIREFOX, isFileExisting, MENU);
 	enableCommand(IDM_VIEW_IN_CHROME, isFileExisting, MENU);
@@ -4435,7 +4436,7 @@ void Notepad_plus::dropFiles(HDROP hdrop)
 		{
 			wchar_t pathDropped[MAX_PATH];
 			::DragQueryFile(hdrop, i, pathDropped, MAX_PATH);
-			if (::PathIsDirectory(pathDropped))
+			if (doesDirectoryExist(pathDropped))
 			{
 				size_t len = lstrlen(pathDropped);
 				if (len > 0 && pathDropped[len - 1] != wchar_t('\\'))
@@ -7040,7 +7041,7 @@ void Notepad_plus::setWorkingDir(const wchar_t *dir)
 	{
 		params.setWorkingDir(NULL);
 	}
-	else if (dir && PathIsDirectory(dir))
+	else if (dir && doesDirectoryExist(dir))
 	{
 		params.setWorkingDir(dir);
 	}
@@ -7795,6 +7796,7 @@ static const QuoteParams quotes[] =
 	{L"Don Ho #3", QuoteParams::rapid, true, SC_CP_UTF8, L_TEXT, L"The smartphone is the best invention of the 21st century for avoiding eye contact with people you know while crossing the street."},
 	{L"Don Ho #4", QuoteParams::rapid, false, SC_CP_UTF8, L_TEXT, L"Poor countries' museums vs. rich countries' museums:\nThe first show what they have left.\nThe second show what they have stolen."},
 	{L"Don Ho #5", QuoteParams::slow, false, SC_CP_UTF8, L_TEXT, L"With great refactoring comes great regressions."},
+	{L"Don Ho #6", QuoteParams::rapid, false, SC_CP_UTF8, L_TEXT, L"Naming a variable always reminds me the effort I put into my existence,\nfor giving some sense to my meaningless life."},
 	{L"Anonymous #1", QuoteParams::slow, false, SC_CP_UTF8, L_TEXT, L"An opinion without 3.14 is just an onion."},
 	{L"Anonymous #2", QuoteParams::rapid, true, SC_CP_UTF8, L_TEXT, L"Before sex, you help each other get naked, after sex you only dress yourself.\nMoral of the story: in life no one helps you once you're fucked."},
 	{L"Anonymous #3", QuoteParams::rapid, false, SC_CP_UTF8, L_TEXT, L"I'm not totally useless. I can be used as a bad example."},
@@ -7817,7 +7819,7 @@ static const QuoteParams quotes[] =
 	{L"Anonymous #20", QuoteParams::slow, false, SC_CP_UTF8, L_TEXT, L"Never make eye contact while eating a banana."},
 	{L"Anonymous #21", QuoteParams::rapid, true, SC_CP_UTF8, L_TEXT, L"I love my sixpack so much, I protect it with a layer of fat."},
 	{L"Anonymous #22", QuoteParams::rapid, true, SC_CP_UTF8, L_TEXT, L"\"It's impossible.\" said pride.\n\"It's risky.\" said experience.\n\"It's pointless.\" said reason.\n\"Give it a try.\" whispered the heart.\n...\n\"What the hell was that?!?!?!?!?!\" shouted the anus two minutes later."},
-	{L"Anonymous #23", QuoteParams::rapid, false, SC_CP_UTF8, L_TEXT, L"A programmer is told to \"go to hell\".\nHe finds the worst part of that statement is the \"go to\"."},
+	{L"Anonymous #23", QuoteParams::rapid, false, SC_CP_UTF8, L_TEXT, L"A C++ programmer is told to \"go to hell\".\nHe finds the most offensive part of that statement is the \"go to\"."},
 	{L"Anonymous #24", QuoteParams::slow, false, SC_CP_UTF8, L_TEXT, L"An Architect's dream is an Engineer's nightmare."},
 	{L"Anonymous #25", QuoteParams::rapid, true, SC_CP_UTF8, L_TEXT, L"In a way, I feel sorry for the kids of this generation.\nThey'll have parents who know how to check browser history."},
 	{L"Anonymous #26", QuoteParams::rapid, true, SC_CP_UTF8, L_TEXT, L"Q: What's the difference between git and github?\nA: It's the difference between porn and pornhub.\n"},

PowerEditor/src/NppCommands.cpp
@@ -595,7 +595,7 @@ void Notepad_plus::command(int id)
 				fullFilePath += L"\"";
 
 				if (id == IDM_EDIT_OPENINFOLDER ||
-					(id == IDM_EDIT_OPENASFILE && !::PathIsDirectory(curentWord)))
+					(id == IDM_EDIT_OPENASFILE && !doesDirectoryExist(curentWord)))
 					::ShellExecute(hwnd, L"open", cmd2Exec, fullFilePath.c_str(), L".", SW_SHOW);
 			}
 			else // Relative file path - need concatenate with current full file path

PowerEditor/src/NppIO.cpp
@@ -262,7 +262,8 @@ BufferID Notepad_plus::doOpen(const wstring& fileName, bool isRecursive, bool is
 	}
 
 	bool isSnapshotMode = backupFileName != NULL && doesFileExist(backupFileName);
-	if (isSnapshotMode && !doesFileExist(longFileName)) // UNTITLED
+	bool longFileNameExists = doesFileExist(longFileName);
+	if (isSnapshotMode && !longFileNameExists) // UNTITLED
 	{
 		wcscpy_s(longFileName, targetFileName.c_str());
 	}
@@ -301,13 +302,13 @@ BufferID Notepad_plus::doOpen(const wstring& fileName, bool isRecursive, bool is
         return foundBufID;
     }
 
-    if (isFileSession(longFileName) && doesFileExist(longFileName))
+    if (isFileSession(longFileName) && longFileNameExists)
     {
         fileLoadSession(longFileName);
         return BUFFER_INVALID;
     }
 
-	if (isFileWorkspace(longFileName) && doesFileExist(longFileName))
+	if (isFileWorkspace(longFileName) && longFileNameExists)
 	{
 		nppParam.setWorkSpaceFilePath(0, longFileName);
 		// This line switches to Project Panel 1 while starting up Npp
@@ -316,12 +317,14 @@ BufferID Notepad_plus::doOpen(const wstring& fileName, bool isRecursive, bool is
 		return BUFFER_INVALID;
 	}
 
+#ifndef	_WIN64
     bool isWow64Off = false;
-    if (!doesFileExist(longFileName))
+    if (!longFileNameExists)
     {
         nppParam.safeWow64EnableWow64FsRedirection(FALSE);
         isWow64Off = true;
     }
+#endif
 
 	bool globbing;
 	if (isRawFileName)
@@ -388,11 +391,13 @@ BufferID Notepad_plus::doOpen(const wstring& fileName, bool isRecursive, bool is
 
 			if (!isCreateFileSuccessful)
 			{
+#ifndef	_WIN64
 				if (isWow64Off)
 				{
 					nppParam.safeWow64EnableWow64FsRedirection(TRUE);
 					isWow64Off = false;
 				}
+#endif
 				return BUFFER_INVALID;
 			}
 		}
@@ -477,7 +482,7 @@ BufferID Notepad_plus::doOpen(const wstring& fileName, bool isRecursive, bool is
     }
     else
     {
-        if (globbing || ::PathIsDirectory(targetFileName.c_str()))
+        if (globbing || doesDirectoryExist(targetFileName.c_str()))
         {
             vector<wstring> fileNames;
             vector<wstring> patterns;
@@ -542,12 +547,13 @@ BufferID Notepad_plus::doOpen(const wstring& fileName, bool isRecursive, bool is
             _pluginsManager.notify(&scnN);
         }
     }
-
+#ifndef	_WIN64
     if (isWow64Off)
     {
         nppParam.safeWow64EnableWow64FsRedirection(TRUE);
         //isWow64Off = false;
     }
+#endif
     return buffer;
 }
 
@@ -659,98 +665,109 @@ bool Notepad_plus::doSave(BufferID id, const wchar_t * filename, bool isCopy)
 	}
 	else if (res == SavingStatus::SaveOpenFailed)
 	{
-		if (_isAdministrator)
+		Buffer* buf = MainFileManager.getBufferByID(id);
+		if (buf->isFromNetwork())
 		{
-			// Already in admin mode? File is probably locked.
 			_nativeLangSpeaker.messageBox("FileLockedWarning",
 				_pPublicInterface->getHSelf(),
-				L"Please check whether if this file is opened in another program",
+				L"Please check whether the network where the file is located is connected.",
 				L"Save failed",
 				MB_OK | MB_ICONWARNING);
 		}
 		else
 		{
-			// try to open Notepad++ in admin mode
-			const NppGUI& nppGui = NppParameters::getInstance().getNppGUI();
-			bool isSnapshotMode = nppGui.isSnapshotMode();
-			bool isAlwaysInMultiInstMode = nppGui._multiInstSetting == multiInst;
-			if (isSnapshotMode && !isAlwaysInMultiInstMode) // if both rememberSession && backup mode are enabled and "Always In Multi-Instance Mode" option not activated:
-			{                                               // Open the 2nd Notepad++ instance in Admin mode, then close the 1st instance.
-
-				int openInAdminModeRes = _nativeLangSpeaker.messageBox("OpenInAdminMode",
-				_pPublicInterface->getHSelf(),
-				L"This file cannot be saved and it may be protected.\rDo you want to launch Notepad++ in Administrator mode?",
-				L"Save failed",
-				MB_YESNO);
-
-				if (openInAdminModeRes == IDYES)
-				{
-					wchar_t nppFullPath[MAX_PATH]{};
-					::GetModuleFileName(NULL, nppFullPath, MAX_PATH);
-
-					wstring args = L"-multiInst";
-					size_t shellExecRes = (size_t)::ShellExecute(_pPublicInterface->getHSelf(), L"runas", nppFullPath, args.c_str(), L".", SW_SHOW);
-
-					// If the function succeeds, it returns a value greater than 32. If the function fails,
-					// it returns an error value that indicates the cause of the failure.
-					// https://msdn.microsoft.com/en-us/library/windows/desktop/bb762153%28v=vs.85%29.aspx
-
-					if (shellExecRes <= 32)
-					{
-						_nativeLangSpeaker.messageBox("OpenInAdminModeFailed",
-							_pPublicInterface->getHSelf(),
-							L"Notepad++ cannot be opened in Administrator mode.",
-							L"Open in Administrator mode failed",
-							MB_OK);
-					}
-					else
-					{
-						::SendMessage(_pPublicInterface->getHSelf(), WM_CLOSE, 0, 0);
-					}
-
-				}
+			if (_isAdministrator)
+			{
+				// Already in admin mode? File is probably locked.
+				_nativeLangSpeaker.messageBox("FileLockedWarning",
+					_pPublicInterface->getHSelf(),
+					L"Please check whether if this file is opened in another program.",
+					L"Save failed",
+					MB_OK | MB_ICONWARNING);
 			}
-			else // rememberSession && backup mode are not both enabled, or "Always In Multi-Instance Mode" option is ON:
-			{    // Open only the file to save in Notepad++ of Administrator mode by keeping the current instance.
+			else
+			{
+				// try to open Notepad++ in admin mode
+				const NppGUI& nppGui = NppParameters::getInstance().getNppGUI();
+				bool isSnapshotMode = nppGui.isSnapshotMode();
+				bool isAlwaysInMultiInstMode = nppGui._multiInstSetting == multiInst;
+				if (isSnapshotMode && !isAlwaysInMultiInstMode) // if both rememberSession && backup mode are enabled and "Always In Multi-Instance Mode" option not activated:
+				{                                               // Open the 2nd Notepad++ instance in Admin mode, then close the 1st instance.
 
-				int openInAdminModeRes = _nativeLangSpeaker.messageBox("OpenInAdminModeWithoutCloseCurrent",
-				_pPublicInterface->getHSelf(),
-				L"The file cannot be saved and it may be protected.\rDo you want to launch Notepad++ in Administrator mode?",
-				L"Save failed",
-				MB_YESNO);
+					int openInAdminModeRes = _nativeLangSpeaker.messageBox("OpenInAdminMode",
+						_pPublicInterface->getHSelf(),
+						L"This file cannot be saved and it may be protected.\rDo you want to launch Notepad++ in Administrator mode?",
+						L"Save failed",
+						MB_YESNO);
 
-				if (openInAdminModeRes == IDYES)
-				{
-					wchar_t nppFullPath[MAX_PATH]{};
-					::GetModuleFileName(NULL, nppFullPath, MAX_PATH);
-
-					BufferID bufferID = _pEditView->getCurrentBufferID();
-					Buffer * buf = MainFileManager.getBufferByID(bufferID);
-
-					//process the fileNamePath into LRF
-					wstring fileNamePath = buf->getFullPathName();
-
-					wstring args = L"-multiInst -nosession ";
-					args += L"\"";
-					args += fileNamePath;
-					args += L"\"";
-					size_t shellExecRes = (size_t)::ShellExecute(_pPublicInterface->getHSelf(), L"runas", nppFullPath, args.c_str(), L".", SW_SHOW);
-
-					// If the function succeeds, it returns a value greater than 32. If the function fails,
-					// it returns an error value that indicates the cause of the failure.
-					// https://msdn.microsoft.com/en-us/library/windows/desktop/bb762153%28v=vs.85%29.aspx
-
-					if (shellExecRes <= 32)
+					if (openInAdminModeRes == IDYES)
 					{
-						_nativeLangSpeaker.messageBox("OpenInAdminModeFailed",
-							_pPublicInterface->getHSelf(),
-							L"Notepad++ cannot be opened in Administrator mode.",
-							L"Open in Administrator mode failed",
-							MB_OK);
+						wchar_t nppFullPath[MAX_PATH]{};
+						::GetModuleFileName(NULL, nppFullPath, MAX_PATH);
+
+						wstring args = L"-multiInst";
+						size_t shellExecRes = (size_t)::ShellExecute(_pPublicInterface->getHSelf(), L"runas", nppFullPath, args.c_str(), L".", SW_SHOW);
+
+						// If the function succeeds, it returns a value greater than 32. If the function fails,
+						// it returns an error value that indicates the cause of the failure.
+						// https://msdn.microsoft.com/en-us/library/windows/desktop/bb762153%28v=vs.85%29.aspx
+
+						if (shellExecRes <= 32)
+						{
+							_nativeLangSpeaker.messageBox("OpenInAdminModeFailed",
+								_pPublicInterface->getHSelf(),
+								L"Notepad++ cannot be opened in Administrator mode.",
+								L"Open in Administrator mode failed",
+								MB_OK);
+						}
+						else
+						{
+							::SendMessage(_pPublicInterface->getHSelf(), WM_CLOSE, 0, 0);
+						}
+
 					}
 				}
-			}
+				else // rememberSession && backup mode are not both enabled, or "Always In Multi-Instance Mode" option is ON:
+				{    // Open only the file to save in Notepad++ of Administrator mode by keeping the current instance.
 
+					int openInAdminModeRes = _nativeLangSpeaker.messageBox("OpenInAdminModeWithoutCloseCurrent",
+						_pPublicInterface->getHSelf(),
+						L"The file cannot be saved and it may be protected.\rDo you want to launch Notepad++ in Administrator mode?",
+						L"Save failed",
+						MB_YESNO);
+
+					if (openInAdminModeRes == IDYES)
+					{
+						wchar_t nppFullPath[MAX_PATH]{};
+						::GetModuleFileName(NULL, nppFullPath, MAX_PATH);
+
+						Buffer* buf = MainFileManager.getBufferByID(id);
+
+						//process the fileNamePath into LRF
+						wstring fileNamePath = buf->getFullPathName();
+
+						wstring args = L"-multiInst -nosession ";
+						args += L"\"";
+						args += fileNamePath;
+						args += L"\"";
+						size_t shellExecRes = (size_t)::ShellExecute(_pPublicInterface->getHSelf(), L"runas", nppFullPath, args.c_str(), L".", SW_SHOW);
+
+						// If the function succeeds, it returns a value greater than 32. If the function fails,
+						// it returns an error value that indicates the cause of the failure.
+						// https://msdn.microsoft.com/en-us/library/windows/desktop/bb762153%28v=vs.85%29.aspx
+
+						if (shellExecRes <= 32)
+						{
+							_nativeLangSpeaker.messageBox("OpenInAdminModeFailed",
+								_pPublicInterface->getHSelf(),
+								L"Notepad++ cannot be opened in Administrator mode.",
+								L"Open in Administrator mode failed",
+								MB_OK);
+						}
+					}
+				}
+
+			}
 		}
 	}
 
@@ -791,26 +808,33 @@ void Notepad_plus::doClose(BufferID id, int whichOne, bool doDeleteBackup)
 	wstring fileFullPath;
 	if (!buf->isUntitled())
 	{
-		// if the file doesn't exist, it could be redirected
-		// So we turn Wow64 off
-		bool isWow64Off = false;
-		NppParameters& nppParam = NppParameters::getInstance();
 		const wchar_t *fn = buf->getFullPathName();
-		if (!doesFileExist(fn))
+		bool fileExists = doesFileExist(fn);
+
+#ifndef	_WIN64
+		// For Notepad++ 32 bits, if the file doesn't exist, it could be redirected
+		// So we turn Wow64 off
+		NppParameters& nppParam = NppParameters::getInstance();
+		bool isWow64Off = false;
+		if (!fileExists)
 		{
 			nppParam.safeWow64EnableWow64FsRedirection(FALSE);
 			isWow64Off = true;
+			fileExists = doesFileExist(fn);
 		}
+#endif
 
-		if (doesFileExist(buf->getFullPathName()))
-			fileFullPath = buf->getFullPathName();
+		if (fileExists)
+			fileFullPath = fn;
 
+#ifndef	_WIN64
 		// We enable Wow64 system, if it was disabled
 		if (isWow64Off)
 		{
 			nppParam.safeWow64EnableWow64FsRedirection(TRUE);
 			//isWow64Off = false;
 		}
+#endif
 	}
 
 	size_t nbDocs = whichOne==MAIN_VIEW?(_mainDocTab.nbItem()):(_subDocTab.nbItem());
@@ -2307,14 +2331,14 @@ bool Notepad_plus::loadSession(Session & session, bool isSnapshotMode, const wch
 			session._mainViewFiles.erase(posIt);
 			continue;	//skip session files, not supporting recursive sessions or embedded workspace files
 		}
-
+#ifndef	_WIN64
 		bool isWow64Off = false;
 		if (!doesFileExist(pFn))
 		{
 			nppParam.safeWow64EnableWow64FsRedirection(FALSE);
 			isWow64Off = true;
 		}
-
+#endif
 		if (doesFileExist(pFn))
 		{
 			if (isSnapshotMode && !session._mainViewFiles[i]._backupFilePath.empty())
@@ -2332,12 +2356,13 @@ bool Notepad_plus::loadSession(Session & session, bool isSnapshotMode, const wch
 			if (foundBufID == BUFFER_INVALID)
 				lastOpened = nppGUI._keepSessionAbsentFileEntries ? MainFileManager.newPlaceholderDocument(pFn, MAIN_VIEW, userCreatedSessionName) : BUFFER_INVALID;
 		}
+#ifndef	_WIN64
 		if (isWow64Off)
 		{
 			nppParam.safeWow64EnableWow64FsRedirection(TRUE);
 			isWow64Off = false;
 		}
-
+#endif
 		if (lastOpened != BUFFER_INVALID)
 		{
 			showView(MAIN_VIEW);
@@ -2437,14 +2462,14 @@ bool Notepad_plus::loadSession(Session & session, bool isSnapshotMode, const wch
 			session._subViewFiles.erase(posIt);
 			continue;	//skip session files, not supporting recursive sessions or embedded workspace files
 		}
-
+#ifndef	_WIN64
 		bool isWow64Off = false;
 		if (!doesFileExist(pFn))
 		{
 			nppParam.safeWow64EnableWow64FsRedirection(FALSE);
 			isWow64Off = true;
 		}
-
+#endif
 		if (doesFileExist(pFn))
 		{
 			//check if already open in main. If so, clone
@@ -2472,12 +2497,13 @@ bool Notepad_plus::loadSession(Session & session, bool isSnapshotMode, const wch
 			if (foundBufID == BUFFER_INVALID)
 				lastOpened = nppGUI._keepSessionAbsentFileEntries ? MainFileManager.newPlaceholderDocument(pFn, SUB_VIEW, userCreatedSessionName) : BUFFER_INVALID;
 		}
-
+#ifndef	_WIN64
 		if (isWow64Off)
 		{
 			nppParam.safeWow64EnableWow64FsRedirection(TRUE);
 			isWow64Off = false;
 		}
+#endif
 
 		if (lastOpened != BUFFER_INVALID)
 		{

commit 484bce5d7998dacfffabf7021142f2472b931a3a

Fix SaveAs dialog "Append extension" option not working issue
Fix the regression introduced in commit:
https://github.com/notepad-plus-plus/notepad-plus-plus/commit/f884a39dd422cda908b36e9439fb00c340c4d85e

Fix https://community.notepad-plus-plus.org/topic/26235/notepad-v8-7-1-release-candidate/4
Close #15745

PowerEditor/src/WinControls/OpenSaveFileDialog/CustomFileDialog.cpp
@@ -505,7 +505,7 @@ private:
 		expandEnv(fileName);
 		bool nameChanged = transformPath(fileName);
 		// Update the controls.
-		if (doesDirectoryExist(getAbsPath(fileName).c_str()))
+		if (!doesDirectoryExist(getAbsPath(fileName).c_str()))
 		{
 			// Name is a file path.
 			// Add file extension if missing.