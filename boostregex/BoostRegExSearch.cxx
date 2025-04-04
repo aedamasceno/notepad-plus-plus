aedamasceno/notepad-plus-plus

commit a52738c1ddaad15fcc56c144402bda5139c13373

Fix count operation on regex makes Notepad++ hang
Fix #16207, close #16208

boostregex/BoostRegExSearch.cxx
@@ -434,7 +434,7 @@ Sci::Position BoostRegexSearch::SearchParameters::nextCharacter(Sci::Position po
 	if (_skip_windows_line_end_as_one_character && _document->CharAt(position) == '\r' && _document->CharAt(position+1) == '\n')
 		return position + 2;
 	else
-		return position + 1;
+		return _document->NextPosition(position, 1);
 }
 
 bool BoostRegexSearch::SearchParameters::isLineStart(Sci::Position position)

commit 46b51237a816d045ea955b5d104716f17db33741

Fix a hanging issue on regexp
The hanging issue is introduced by https://github.com/notepad-plus-plus/notepad-plus-plus/commit/a52738c1ddaad15fcc56c144402bda5139c13373.

Close #16371

boostregex/BoostRegExSearch.cxx
@@ -437,7 +437,7 @@ Sci::Position BoostRegexSearch::SearchParameters::nextCharacter(Sci::Position po
 	if (_skip_windows_line_end_as_one_character && _document->CharAt(position) == '\r' && _document->CharAt(position+1) == '\n')
 		return position + 2;
 	else
-		return _document->NextPosition(position, 1);
+		return std::max(_document->NextPosition(position, 1), position + 1);
 }
 
 bool BoostRegexSearch::SearchParameters::isLineStart(Sci::Position position) const