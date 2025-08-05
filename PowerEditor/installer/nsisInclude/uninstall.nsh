aedamasceno/notepad-plus-plus

commit 889efad2e80511a5783882728640dec6ce35ce15

Fix uninstaller security issue (CVE-2025-49144)
Fix #16787, close #16788

PowerEditor/installer/nsisInclude/uninstall.nsh
@@ -55,12 +55,12 @@ FunctionEnd
 
 
 Section un.explorerContextMenu
-	ExecWait 'regsvr32 /u /s "$INSTDIR\NppShell_01.dll"'
-	ExecWait 'regsvr32 /u /s "$INSTDIR\NppShell_02.dll"'
-	ExecWait 'regsvr32 /u /s "$INSTDIR\NppShell_03.dll"'
-	ExecWait 'regsvr32 /u /s "$INSTDIR\NppShell_04.dll"'
-	ExecWait 'regsvr32 /u /s "$INSTDIR\NppShell_05.dll"'
-	ExecWait 'regsvr32 /u /s "$INSTDIR\NppShell_06.dll"'
+	ExecWait '"$winSysDir\regsvr32.exe" /u /s "$INSTDIR\NppShell_01.dll"'
+	ExecWait '"$winSysDir\regsvr32.exe" /u /s "$INSTDIR\NppShell_02.dll"'
+	ExecWait '"$winSysDir\regsvr32.exe" /u /s "$INSTDIR\NppShell_03.dll"'
+	ExecWait '"$winSysDir\regsvr32.exe" /u /s "$INSTDIR\NppShell_04.dll"'
+	ExecWait '"$winSysDir\regsvr32.exe" /u /s "$INSTDIR\NppShell_05.dll"'
+	ExecWait '"$winSysDir\regsvr32.exe" /u /s "$INSTDIR\NppShell_06.dll"'
 	Delete "$INSTDIR\NppShell_01.dll"
 	Delete "$INSTDIR\NppShell_02.dll"
 	Delete "$INSTDIR\NppShell_03.dll"
@@ -68,7 +68,7 @@ Section un.explorerContextMenu
 	Delete "$INSTDIR\NppShell_05.dll"
 	Delete "$INSTDIR\NppShell_06.dll"
 	
-	ExecWait 'regsvr32 /u /s "$INSTDIR\contextmenu\NppShell.dll"'
+	ExecWait '"$winSysDir\regsvr32.exe" /u /s "$INSTDIR\contextmenu\NppShell.dll"'
 SectionEnd
 
 Section un.UnregisterFileExt
@@ -295,7 +295,7 @@ Section Uninstall
 	; In order to not delete context menu binary before we unregistered it,
 	; we delete them at the end, using the CleanupDll function, since it can be locked by explorer.
 	IfFileExists "$INSTDIR\contextmenu\NppShell.dll" 0 +2
-		ExecWait 'rundll32.exe "$INSTDIR\contextmenu\NppShell.dll",CleanupDll'
+		ExecWait '"$winSysDir\rundll32.exe" "$INSTDIR\contextmenu\NppShell.dll",CleanupDll'
 	Delete "$INSTDIR\contextmenu\NppShell.msix"
 	
 	

commit 98c23953d5e2f98462ad5fa7ab72369f842ede54

Fix "Edit with Notepad++" not removed by uninstaller regression (from v8.8.3)
The regression was introduced by:
https://github.com/notepad-plus-plus/notepad-plus-plus/commit/889efad2e80511a5783882728640dec6ce35ce15#commitcomment-161831592

Fix #16896

PowerEditor/installer/nsisInclude/uninstall.nsh
@@ -17,12 +17,21 @@
 Var installPath
 Var doLocalConf
 Var keepUserData
+
 Function un.onInit
+
+!ifdef ARCH64 || ARCHARM64 ; x64 or ARM64 : installation of 64 bits Notepad++ & its 64 bits components
+	StrCpy $winSysDir "$WINDIR\System32"
+!else ; installation of 32 bits Notepad++ & its 32 bits components
+	StrCpy $winSysDir "$WINDIR\SysWOW64"
+!endif
+
 	StrCpy $keepUserData "false"	; default value(It is must, otherwise few files such as shortcuts.xml, contextMenu.xml etc, will not be removed when $INSTDIR\doLocalConf.xml is not available.)
 	; determinate theme path for uninstall themes
 	StrCpy $installPath "$APPDATA\${APPNAME}"
 	StrCpy $doLocalConf "false"
 	IfFileExists $INSTDIR\doLocalConf.xml doesExist noneExist
+
 doesExist:
 	StrCpy $installPath $INSTDIR
 	StrCpy $doLocalConf "true"