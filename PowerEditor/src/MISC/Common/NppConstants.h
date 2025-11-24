aedamasceno/notepad-plus-plus

commit 7da0c061d980c23f147340324883f0463a24ce1e

Use modern subclassing on some controls
- string dialog edit control
- document map
- preference dialog edit control for vertical edge
- document list list view
- document switcher
- hash dialog edit controls

Fix #17158, close #17159

PowerEditor/src/WinControls/DocumentMap/documentMap.cpp
@@ -16,6 +16,10 @@
 
 
 #include "documentMap.h"
+
+#include <commctrl.h>
+
+#include "NppConstants.h"
 #include "ScintillaEditView.h"
 
 
@@ -498,15 +502,14 @@ void ViewZoneDlg::doDialog()
 
 intptr_t CALLBACK ViewZoneDlg::run_dlgProc(UINT message, WPARAM wParam, LPARAM lParam)
 {
-	switch (message) 
+	switch (message)
 	{
-        case WM_INITDIALOG :
+		case WM_INITDIALOG:
 		{
 			_viewZoneCanvas = ::GetDlgItem(_hSelf, IDC_VIEWZONECANVAS);
-			if (NULL != _viewZoneCanvas)
+			if (_viewZoneCanvas != nullptr)
 			{
-				::SetWindowLongPtr(_viewZoneCanvas, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
-				_canvasDefaultProc = reinterpret_cast<WNDPROC>(::SetWindowLongPtr(_viewZoneCanvas, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(canvasStaticProc)));
+				::SetWindowSubclass(_viewZoneCanvas, ViewZoneDlg::CanvasProc, static_cast<UINT_PTR>(SubclassID::first), 0);
 				return TRUE;
 			}
 			break;
@@ -525,9 +528,9 @@ intptr_t CALLBACK ViewZoneDlg::run_dlgProc(UINT message, WPARAM wParam, LPARAM l
 			break;
 		}
 
-		case WM_DRAWITEM :
+		case WM_DRAWITEM:
 		{
-			drawPreviewZone((DRAWITEMSTRUCT *)lParam);
+			drawPreviewZone(reinterpret_cast<DRAWITEMSTRUCT*>(lParam));
 			return TRUE;
 		}
 
@@ -557,55 +560,47 @@ intptr_t CALLBACK ViewZoneDlg::run_dlgProc(UINT message, WPARAM wParam, LPARAM l
 	return FALSE;
 }
 
-LRESULT CALLBACK ViewZoneDlg::canvasStaticProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
+LRESULT CALLBACK ViewZoneDlg::CanvasProc(
+	HWND hWnd,
+	UINT uMsg,
+	WPARAM wParam,
+	LPARAM lParam,
+	UINT_PTR uIdSubclass,
+	[[maybe_unused]] DWORD_PTR /*dwRefData*/
+)
 {
-	ViewZoneDlg *pViewZoneDlg = reinterpret_cast<ViewZoneDlg *>(::GetWindowLongPtr(hwnd, GWLP_USERDATA));
-	if (!pViewZoneDlg)
-		return FALSE;
-	return pViewZoneDlg->canvas_runProc(hwnd, message, wParam, lParam);
-}
-
-LRESULT CALLBACK ViewZoneDlg::canvas_runProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	switch (message)
-    {
-		case WM_DESTROY:
+	switch (uMsg)
+	{
+		case WM_NCDESTROY:
 		{
-			//::MessageBoxA(NULL,"Destroy","",MB_OK);
+			::RemoveWindowSubclass(hWnd, ViewZoneDlg::CanvasProc, uIdSubclass);
+			break;
 		}
-		return TRUE;
 
 		case WM_KEYDOWN:
+		{
+			HWND hParent = ::GetParent(hWnd);
 			if (wParam == VK_UP)
 			{
-				::SendMessage(_hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveUp), 0);
+				::SendMessage(hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveUp), 0);
 			}
 			if (wParam == VK_DOWN)
 			{
-				::SendMessage(_hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveDown), 0);
+				::SendMessage(hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveDown), 0);
 			}
 			if (wParam == VK_PRIOR)
 			{
-				::SendMessage(_hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveUp), 1);
+				::SendMessage(hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveUp), 1);
 			}
 			if (wParam == VK_NEXT)
 			{
-				::SendMessage(_hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveDown), 1);
+				::SendMessage(hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveDown), 1);
 			}
 			break;
-
-        case WM_SIZE:
-        {
-            break;
-        }
-
-		case WM_NOTIFY:
-		{
 		}
-		return TRUE;
 
-        default :
-            return _canvasDefaultProc(hwnd, message, wParam, lParam);
-    }
-	return _canvasDefaultProc(hwnd, message, wParam, lParam);
+		 default:
+			break;
+	}
+	return ::DefSubclassProc(hWnd, uMsg, wParam, lParam);
 }

PowerEditor/src/WinControls/DocumentMap/documentMap.h
@@ -44,7 +44,7 @@ enum moveMode {
 class ViewZoneDlg : public StaticDialog
 {
 public :
-	ViewZoneDlg() : StaticDialog(), _viewZoneCanvas(nullptr), _canvasDefaultProc(nullptr), _higherY(0), _lowerY(0) {}
+	ViewZoneDlg() : StaticDialog(), _viewZoneCanvas(nullptr), _higherY(0), _lowerY(0) {}
 
 	enum class ViewZoneColorIndex {
 		focus,
@@ -75,9 +75,6 @@ public :
 protected :
 	intptr_t CALLBACK run_dlgProc(UINT message, WPARAM wParam, LPARAM lParam) override;
 
-	static LRESULT CALLBACK canvasStaticProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
-	LRESULT CALLBACK canvas_runProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
-
 	static COLORREF _focus;
 	static COLORREF _frost;
 
@@ -85,10 +82,11 @@ protected :
 
 private :
 	HWND _viewZoneCanvas = nullptr;
-	WNDPROC _canvasDefaultProc = nullptr;
 	
 	long _higherY = 0;
 	long _lowerY = 0;
+
+	static LRESULT CALLBACK CanvasProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, UINT_PTR uIdSubclass, DWORD_PTR dwRefData);
 };
 
 

PowerEditor/src/MISC/md5/md5Dlgs.cpp
@@ -15,7 +15,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "md5.h"
-#include <stdint.h>
+#include <cstdint>
 #include "sha-256.h"
 #include "sha512.h"
 #include "calc_sha1.h"
@@ -25,6 +25,13 @@
 #include "Parameters.h"
 #include <shlwapi.h>
 #include "resource.h"
+
+#include <commctrl.h>
+
+#include "NppConstants.h"
+#include "NppDarkMode.h"
+
+static LRESULT CALLBACK TextEditSelectAllProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, UINT_PTR uIdSubclass, DWORD_PTR dwRefData);
 
 intptr_t CALLBACK HashFromFilesDlg::run_dlgProc(UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -42,11 +49,8 @@ intptr_t CALLBACK HashFromFilesDlg::run_dlgProc(UINT message, WPARAM wParam, LPA
 			::SendMessage(hHashPathEdit, WM_SETFONT, reinterpret_cast<WPARAM>(_hFont), TRUE);
 			::SendMessage(hHashResult, WM_SETFONT, reinterpret_cast<WPARAM>(_hFont), TRUE);
 
-			::SetWindowLongPtr(hHashPathEdit, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
-			_oldHashPathEditProc = reinterpret_cast<WNDPROC>(::SetWindowLongPtr(hHashPathEdit, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(HashPathEditStaticProc)));
-
-			::SetWindowLongPtr(hHashResult, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
-			_oldHashResultProc = reinterpret_cast<WNDPROC>(::SetWindowLongPtr(hHashResult, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(HashResultStaticProc)));
+			::SetWindowSubclass(hHashPathEdit, TextEditSelectAllProc, static_cast<UINT_PTR>(SubclassID::first), 0);
+			::SetWindowSubclass(hHashResult, TextEditSelectAllProc, static_cast<UINT_PTR>(SubclassID::first), 0);
 		}
 		return TRUE;
 
@@ -225,21 +229,34 @@ intptr_t CALLBACK HashFromFilesDlg::run_dlgProc(UINT message, WPARAM wParam, LPA
 	return FALSE;
 }
 
-LRESULT run_textEditProc(WNDPROC oldEditProc, HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+static LRESULT CALLBACK TextEditSelectAllProc(
+	HWND hWnd,
+	UINT uMsg,
+	WPARAM wParam,
+	LPARAM lParam,
+	UINT_PTR uIdSubclass,
+	[[maybe_unused]] DWORD_PTR /*dwRefData*/
+)
 {
-	switch (message)
+	switch (uMsg)
 	{
+		case WM_NCDESTROY:
+		{
+			::RemoveWindowSubclass(hWnd, TextEditSelectAllProc, uIdSubclass);
+			break;
+		}
+
 		case WM_GETDLGCODE:
 		{
-			return DLGC_WANTALLKEYS | ::CallWindowProc(oldEditProc, hwnd, message, wParam, lParam);
+			return DLGC_WANTALLKEYS | ::DefSubclassProc(hWnd, uMsg, wParam, lParam);
 		}
 
 		case WM_CHAR:
 		{
 			if (wParam == 1) // Ctrl+A
 			{
-				::SendMessage(hwnd, EM_SETSEL, 0, -1);
-				return TRUE;
+				::SendMessage(hWnd, EM_SETSEL, 0, -1);
+				return 0;
 			}
 			break;
 		}
@@ -247,7 +264,7 @@ LRESULT run_textEditProc(WNDPROC oldEditProc, HWND hwnd, UINT message, WPARAM wP
 		default:
 			break;
 	}
-	return ::CallWindowProc(oldEditProc, hwnd, message, wParam, lParam);
+	return ::DefSubclassProc(hWnd, uMsg, wParam, lParam);
 }
 
 void HashFromFilesDlg::setHashType(hashType hashType2set)
@@ -485,11 +502,8 @@ intptr_t CALLBACK HashFromTextDlg::run_dlgProc(UINT message, WPARAM wParam, LPAR
 			::SendMessage(hHashTextEdit, WM_SETFONT, reinterpret_cast<WPARAM>(_hFont), TRUE);
 			::SendMessage(hHashResult, WM_SETFONT, reinterpret_cast<WPARAM>(_hFont), TRUE);
 
-			::SetWindowLongPtr(hHashTextEdit, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
-			_oldHashTextEditProc = reinterpret_cast<WNDPROC>(::SetWindowLongPtr(hHashTextEdit, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(HashTextEditStaticProc)));
-
-			::SetWindowLongPtr(hHashResult, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
-			_oldHashResultProc = reinterpret_cast<WNDPROC>(::SetWindowLongPtr(hHashResult, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(HashResultStaticProc)));
+			::SetWindowSubclass(hHashTextEdit, TextEditSelectAllProc, static_cast<UINT_PTR>(SubclassID::first), 0);
+			::SetWindowSubclass(hHashResult, TextEditSelectAllProc, static_cast<UINT_PTR>(SubclassID::first), 0);
 		}
 		return TRUE;
 

PowerEditor/src/MISC/md5/md5Dlgs.h
@@ -23,8 +23,6 @@ enum hashType {hash_md5 = 16, hash_sha1 = 20, hash_sha256 = 32, hash_sha512 = 64
 #define HASH_MAX_LENGTH hash_sha512
 #define HASH_STR_MAX_LENGTH (hash_sha512 * 2 + 1)
 
-LRESULT run_textEditProc(WNDPROC oldEditProc, HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
-
 class HashFromFilesDlg : public StaticDialog
 {
 public :
@@ -38,19 +36,7 @@ protected :
 	intptr_t CALLBACK run_dlgProc(UINT message, WPARAM wParam, LPARAM lParam) override;
 	hashType _ht = hash_md5;
 
-	static LRESULT CALLBACK HashPathEditStaticProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-		const auto dlg = (HashFromFilesDlg *)(::GetWindowLongPtr(hwnd, GWLP_USERDATA));
-		return (run_textEditProc(dlg->_oldHashPathEditProc, hwnd, message, wParam, lParam));
-	}
-
-	static LRESULT CALLBACK HashResultStaticProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-		const auto dlg = (HashFromFilesDlg *)(::GetWindowLongPtr(hwnd, GWLP_USERDATA));
-		return (run_textEditProc(dlg->_oldHashResultProc, hwnd, message, wParam, lParam));
-	}
-
 private :
-	WNDPROC _oldHashPathEditProc = nullptr;
-	WNDPROC _oldHashResultProc = nullptr;
 	HFONT _hFont = nullptr;
 };
 
@@ -69,18 +55,6 @@ protected :
 	intptr_t CALLBACK run_dlgProc(UINT message, WPARAM wParam, LPARAM lParam) override;
 	hashType _ht = hash_md5;
 
-	static LRESULT CALLBACK HashTextEditStaticProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-		const auto dlg = (HashFromTextDlg *)(::GetWindowLongPtr(hwnd, GWLP_USERDATA));
-		return (run_textEditProc(dlg->_oldHashTextEditProc, hwnd, message, wParam, lParam));
-	}
-
-	static LRESULT CALLBACK HashResultStaticProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-		const auto dlg = (HashFromTextDlg *)(::GetWindowLongPtr(hwnd, GWLP_USERDATA));
-		return (run_textEditProc(dlg->_oldHashResultProc, hwnd, message, wParam, lParam));
-	}
-
 private :
-	WNDPROC _oldHashTextEditProc = nullptr;
-	WNDPROC _oldHashResultProc = nullptr;
 	HFONT _hFont = nullptr;
 };

PowerEditor/src/ScintillaComponent/UserDefineDialog.cpp
@@ -24,6 +24,10 @@
 #include "CustomFileDialog.h"
 //#include "Common.h"
 
+#include <commctrl.h>
+
+#include "NppConstants.h"
+
 using namespace std;
 
 UserLangContainer * SharedParametersDialog::_pUserLang = NULL;
@@ -1595,10 +1599,9 @@ intptr_t CALLBACK StringDlg::run_dlgProc(UINT Message, WPARAM wParam, LPARAM lPa
 			NppDarkMode::autoSubclassAndThemeChildControls(_hSelf);
 
 			// Re-route to Subclassed the edit control's proc if needed
-			if (_restrictedChars.length())
+			if (!_restrictedChars.empty())
 			{
-				::SetWindowLongPtr(GetDlgItem(_hSelf, IDC_STRING_EDIT), GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
-				_oldEditProc = reinterpret_cast<WNDPROC>(SetWindowLongPtr(GetDlgItem(_hSelf, IDC_STRING_EDIT), GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(customEditProc)));
+				::SetWindowSubclass(::GetDlgItem(_hSelf, IDC_STRING_EDIT), CustomEditProc, static_cast<UINT_PTR>(SubclassID::first), reinterpret_cast<DWORD_PTR>(this));
 			}
 
             ::SetWindowText(_hSelf, _title.c_str());
@@ -1685,48 +1688,55 @@ intptr_t CALLBACK StringDlg::run_dlgProc(UINT Message, WPARAM wParam, LPARAM lPa
 	return FALSE;
 }
 
-LRESULT StringDlg::customEditProc(HWND hEdit, UINT msg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK StringDlg::CustomEditProc(
+	HWND hWnd,
+	UINT uMsg,
+	WPARAM wParam,
+	LPARAM lParam,
+	UINT_PTR uIdSubclass,
+	DWORD_PTR dwRefData
+)
 {
-	StringDlg *pSelf = reinterpret_cast<StringDlg *>(::GetWindowLongPtr(hEdit, GWLP_USERDATA));
-	if (!pSelf)
-	{
-		return 0;
-	}
+	auto* pRefData = reinterpret_cast<StringDlg*>(dwRefData);
 
-	switch (msg)
+	switch (uMsg)
 	{
-	case WM_CHAR:
-		if (0x80 & GetKeyState(VK_CONTROL))
+		case WM_NCDESTROY:
 		{
-			switch (wParam)
+			::RemoveWindowSubclass(hWnd, StringDlg::CustomEditProc, uIdSubclass);
+			break;
+		}
+
+		case WM_CHAR:
+		{
+			if (0x80 & GetKeyState(VK_CONTROL))
 			{
-			case 0x16: // ctrl - V
-				pSelf->HandlePaste(hEdit);
-				return 0;
+				switch (wParam)
+				{
+					case 0x16: // ctrl - V
+						pRefData->HandlePaste(hWnd);
+						return 0;
 
-			case 0x03: // ctrl - C
-			case 0x18: // ctrl - X
-			default:
-				// Let them go to default
-				break;
+					case 0x03: // ctrl - C
+					case 0x18: // ctrl - X
+					default:
+						// Let them go to default
+						break;
+				}
 			}
+			else
+			{
+				// If Key pressed not permitted, then return 0
+				if (!pRefData->isAllowed(reinterpret_cast<wchar_t*>(&wParam)))
+					return 0;
+			}
+			break;
 		}
-		else
-		{
-			// If Key pressed not permitted, then return 0
-			if (!pSelf->isAllowed(reinterpret_cast<wchar_t*>(&wParam)))
-				return 0;
-		}
-		break;
 
-	case WM_DESTROY:
-		// Reset the message handler to the original one
-		SetWindowLongPtr(hEdit, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(pSelf->_oldEditProc));
-		return 0;
+		default:
+			break;
 	}
-
-	// Process the message using the default handler
-	return CallWindowProc(pSelf->_oldEditProc, hEdit, msg, wParam, lParam);
+	return ::DefSubclassProc(hWnd, uMsg, wParam, lParam);
 }
 
 bool StringDlg::isAllowed([[maybe_unused]] const wstring & txt)

PowerEditor/src/ScintillaComponent/UserDefineDialog.h
@@ -408,7 +408,7 @@ protected :
     intptr_t CALLBACK run_dlgProc(UINT Message, WPARAM wParam, LPARAM) override;
 
 	// Custom proc to subclass edit control
-	LRESULT static CALLBACK customEditProc(HWND hEdit, UINT msg, WPARAM wParam, LPARAM lParam);
+	static LRESULT CALLBACK CustomEditProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, UINT_PTR uIdSubclass, DWORD_PTR dwRefData);
 
 	bool isAllowed(const std::wstring& txt);
 	void HandlePaste(HWND hEdit);
@@ -420,7 +420,6 @@ private :
 	std::wstring _restrictedChars;
     int _txtLen = 0;
 	bool _shouldGotoCenter = false;
-	WNDPROC _oldEditProc = nullptr;
 };
 
 class StylerDlg : public StaticDialog

PowerEditor/src/WinControls/Preference/preferenceDlg.cpp
@@ -16,11 +16,14 @@
 
 #include <shlwapi.h>
 #include "preferenceDlg.h"
-#include "lesDlgs.h"
 #include "EncodingMapper.h"
 #include "localization.h"
 #include <algorithm>
 #include "ScintillaEditView.h"
+
+#include <commctrl.h>
+
+#include "NppConstants.h"
 
 
 #define MyGetGValue(rgb)      (LOBYTE((rgb)>>8))
@@ -1756,12 +1759,24 @@ bool hasOnlyNumSpaceInClipboard()
 	return true;
 }
 
-static WNDPROC oldFunclstToolbarProc = NULL;
-static LRESULT CALLBACK editNumSpaceProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+static LRESULT CALLBACK EditNumSpaceProc(
+	HWND hWnd,
+	UINT uMsg,
+	WPARAM wParam,
+	LPARAM lParam,
+	UINT_PTR uIdSubclass,
+	[[maybe_unused]] DWORD_PTR /*dwRefData*/
+)
 {
 	static bool canPaste = false;
-	switch (message)
+	switch (uMsg)
 	{
+		case WM_NCDESTROY:
+		{
+			::RemoveWindowSubclass(hWnd, EditNumSpaceProc, uIdSubclass);
+			break;
+		}
+
 		case WM_KEYDOWN:
 		{
 			bool ctrl = GetKeyState(VK_CONTROL) & 0x8000;
@@ -1775,7 +1790,7 @@ static LRESULT CALLBACK editNumSpaceProc(HWND hwnd, UINT message, WPARAM wParam,
 				canPaste = hasOnlyNumSpaceInClipboard();
 
 				if (shift_INS && !canPaste) // Shift-INS is different from Ctrl-V, it doesn't pass by WM_CHAR afterward, so we stop here
-					return TRUE;
+					return 0;
 			}
 		}
 		break;
@@ -1797,7 +1812,7 @@ static LRESULT CALLBACK editNumSpaceProc(HWND hwnd, UINT message, WPARAM wParam,
 			{
 				if (!canPaste) // it comes from Ctrl-V of WM_KEYDOWN: the format is not correct or nothing to paste, so stop here
 				{
-					return TRUE;
+					return 0;
 				}
 				else
 				{
@@ -1812,7 +1827,7 @@ static LRESULT CALLBACK editNumSpaceProc(HWND hwnd, UINT message, WPARAM wParam,
 			{
 				if (wParam != VK_BACK && wParam != ' ' && (wParam < '0' || wParam > '9')) // If input char is not number or white space, stop here
 				{
-					return TRUE;
+					return 0;
 				}
 			}
 		}
@@ -1821,7 +1836,7 @@ static LRESULT CALLBACK editNumSpaceProc(HWND hwnd, UINT message, WPARAM wParam,
 		default:
 			break;
 	}
-	return oldFunclstToolbarProc(hwnd, message, wParam, lParam);
+	return ::DefSubclassProc(hWnd, uMsg, wParam, lParam);
 }
 
 
@@ -1829,7 +1844,7 @@ intptr_t CALLBACK EditingSubDlg::run_dlgProc(UINT message, WPARAM wParam, LPARAM
 {
 	NppParameters& nppParam = NppParameters::getInstance();
 	NppGUI & nppGUI = nppParam.getNppGUI();
-	ScintillaViewParams& svp = (ScintillaViewParams&)nppParam.getSVP();
+	auto& svp = const_cast<ScintillaViewParams&>(nppParam.getSVP());
 	
 	switch (message)
 	{
@@ -2961,7 +2976,7 @@ void MarginsBorderEdgeSubDlg::initScintParam()
 	}
 	::SendDlgItemMessage(_hSelf, IDC_COLUMNPOS_EDIT, WM_SETTEXT, 0, reinterpret_cast<LPARAM>(edgeColumnPosStr.c_str()));
 
-	oldFunclstToolbarProc = reinterpret_cast<WNDPROC>(::SetWindowLongPtr(::GetDlgItem(_hSelf, IDC_COLUMNPOS_EDIT), GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(editNumSpaceProc)));
+	::SetWindowSubclass(::GetDlgItem(_hSelf, IDC_COLUMNPOS_EDIT), EditNumSpaceProc, static_cast<UINT_PTR>(SubclassID::first), 0);
 }
 
 intptr_t CALLBACK MarginsBorderEdgeSubDlg::run_dlgProc(UINT message, WPARAM wParam, LPARAM lParam)

commit 39a659ec2a29f9c056da575c5f040634589b890d

Fix document map regression with keyboard input
- fix some warnings
- optimize document map global variables

Regression is introduced by: https://github.com/notepad-plus-plus/notepad-plus-plus/commit/7da0c061d980c23f147340324883f0463a24ce1e

Fix #17224, close #17225

PowerEditor/src/MISC/Common/NppConstants.h
@@ -16,6 +16,8 @@
 
 #pragma once
 
+#include <windows.h>
+
 inline constexpr bool dirUp = true;
 inline constexpr bool dirDown = false;
 
@@ -118,3 +120,9 @@ enum NumBase : unsigned char
 };
 
 // ScintillaEditView
+
+// documentMap
+
+inline constexpr UINT DOCUMENTMAP_MOUSEWHEEL = (WM_USER + 3);
+
+// documentMap

PowerEditor/src/ScintillaComponent/ScintillaEditView.cpp
@@ -509,6 +509,9 @@ LRESULT CALLBACK ScintillaEditView::scintillaStatic_Proc(HWND hwnd, UINT Message
 {
 	ScintillaEditView *pScint = (ScintillaEditView *)(::GetWindowLongPtr(hwnd, GWLP_USERDATA));
 
+	if (Message == DOCUMENTMAP_MOUSEWHEEL)
+		return ::CallWindowProc(_scintillaDefaultProc, hwnd, WM_MOUSEWHEEL, wParam, lParam);
+
 	if (Message == WM_MOUSEWHEEL || Message == WM_MOUSEHWHEEL)
 	{
 		POINT pt{};

PowerEditor/src/ScintillaComponent/ScintillaEditView.h
@@ -643,10 +643,6 @@ public:
 	void setCRLF(long color = -1);
 	void setNpcAndCcUniEOL(long color = -1);
 
-	void mouseWheel(WPARAM wParam, LPARAM lParam) {
-		scintillaNew_Proc(_hSelf, WM_MOUSEWHEEL, wParam, lParam);
-	}
-
 	void setTabSettings(Lang* lang);
 	bool isWrapRestoreNeeded() const { return _wrapRestoreNeeded; }
 	void setWrapRestoreNeeded(bool isWrapRestoreNeeded) { _wrapRestoreNeeded = isWrapRestoreNeeded; }

PowerEditor/src/WinControls/DocumentMap/documentMap.cpp
@@ -23,6 +23,12 @@
 #include "ScintillaEditView.h"
 
 
+#define DOCUMENTMAP_SCROLL        (WM_USER + 4) // DM_SETDEFID uses WM_USER + 1
+#define DOCUMENTMAP_MOUSECLICKED  (WM_USER + 5) // DM_REPOSITION uses WM_USER + 2
+
+static constexpr bool moveDown = true;
+static constexpr bool moveUp = false;
+
 void DocumentMap::reloadMap()
 {
 	if (_pMapView && _ppEditView)
@@ -156,8 +162,9 @@ sprintf(dchar, "%f", ddd);
 		// 19     => 11.5
 		// 20     => 12
 */
-double zoomRatio[] = {1, 1, 1, 1, 1.5, 2, 2.5, 2.5, 3.5, 3.5,\
-4, 4.5, 5, 5, 5.5, 6, 6.5, 7, 7, 7.5, 8, 8.5, 8.5, 9.5, 9.5, 10, 10.5, 11, 11, 11.5, 12};
+
+static constexpr double zoomRatio[]{ 1, 1, 1, 1, 1.5, 2, 2.5, 2.5, 3.5, 3.5,
+4, 4.5, 5, 5, 5.5, 6, 6.5, 7, 7, 7.5, 8, 8.5, 8.5, 9.5, 9.5, 10, 10.5, 11, 11, 11.5, 12 };
 
 void DocumentMap::wrapMap(const ScintillaEditView *editView)
 {
@@ -377,7 +384,7 @@ intptr_t CALLBACK DocumentMap::run_dlgProc(UINT message, WPARAM wParam, LPARAM l
 
 		case WM_NOTIFY:
 		{
-			switch (((LPNMHDR)lParam)->code)
+			switch (reinterpret_cast<LPNMHDR>(lParam)->code)
 			{
 				case DMN_CLOSE:
 				{
@@ -440,13 +447,13 @@ intptr_t CALLBACK DocumentMap::run_dlgProc(UINT message, WPARAM wParam, LPARAM l
 
 		case DOCUMENTMAP_MOUSEWHEEL:
 		{
-			(*_ppEditView)->mouseWheel(wParam, lParam);
+			::SendMessage((*_ppEditView)->getHSelf(), DOCUMENTMAP_MOUSEWHEEL, wParam, lParam);
+			return TRUE;
 		}
-		return TRUE;
 
-        default :
-            return DockingDlgInterface::run_dlgProc(message, wParam, lParam);
-    }
+		default:
+			break;
+	}
 	return DockingDlgInterface::run_dlgProc(message, wParam, lParam);
 }
 
@@ -579,22 +586,35 @@ LRESULT CALLBACK ViewZoneDlg::CanvasProc(
 
 		case WM_KEYDOWN:
 		{
-			HWND hParent = ::GetParent(hWnd);
-			if (wParam == VK_UP)
+			HWND hParent = ::GetParent(::GetParent(hWnd));
+			switch (wParam)
 			{
-				::SendMessage(hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveUp), 0);
-			}
-			if (wParam == VK_DOWN)
-			{
-				::SendMessage(hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveDown), 0);
-			}
-			if (wParam == VK_PRIOR)
-			{
-				::SendMessage(hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveUp), 1);
-			}
-			if (wParam == VK_NEXT)
-			{
-				::SendMessage(hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveDown), 1);
+				case VK_UP:
+				{
+					::SendMessage(hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveUp), 0);
+					break;
+				}
+
+				case VK_DOWN:
+				{
+					::SendMessage(hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveDown), 0);
+					break;
+				}
+
+				case VK_PRIOR:
+				{
+					::SendMessage(hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveUp), 1);
+					break;
+				}
+
+				case VK_NEXT:
+				{
+					::SendMessage(hParent, DOCUMENTMAP_SCROLL, static_cast<WPARAM>(moveDown), 1);
+					break;
+				}
+
+				default:
+					break;
 			}
 			break;
 		}

PowerEditor/src/WinControls/DocumentMap/documentMap.h
@@ -22,19 +22,11 @@
 
 #define DM_PANELTITLE     L"Document Map"
 
-#define DOCUMENTMAP_SCROLL        (WM_USER + 4) // DM_SETDEFID uses WM_USER + 1
-#define DOCUMENTMAP_MOUSECLICKED  (WM_USER + 5) // DM_REPOSITION uses WM_USER + 2
-#define DOCUMENTMAP_MOUSEWHEEL    (WM_USER + 3)
-
 const wchar_t VIEWZONE_DOCUMENTMAP[64] = L"Document map";
 
 class ScintillaEditView;
 class Buffer;
 struct MapPosition;
-
-const bool moveDown = true;
-const bool moveUp = false;
-
 
 enum moveMode {
 	perLine,
@@ -154,4 +146,6 @@ private:
 	intptr_t _displayZoom = -1;
 	intptr_t _displayWidth = 0;
 	std::wstring id4dockingCont = DM_NOFOCUSWHILECLICKINGCAPTION;
+
+	using DockingDlgInterface::init;
 };