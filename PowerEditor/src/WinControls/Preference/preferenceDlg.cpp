aedamasceno/notepad-plus-plus

commit c4ee1aff9ccc559ec94fdff0e7650a91277483ec

Fix empty sub-menus in compact language menu issue
Fix #18202, close #18231

PowerEditor/src/WinControls/Preference/preferenceDlg.cpp
@@ -4817,26 +4817,69 @@ intptr_t CALLBACK LanguageSubDlg::run_dlgProc(UINT message, WPARAM wParam, LPARA
 					}
 
 					HWND grandParent = ::GetParent(_hParent);
+					HMENU mainMenu = reinterpret_cast<HMENU>(::SendMessage(grandParent, NPPM_INTERNAL_GETMENU, 0, 0));
 
 					if (LOWORD(wParam)==IDC_BUTTON_REMOVE)
 					{
-						HMENU menu2remove = reinterpret_cast<HMENU>(::SendMessage(grandParent, NPPM_INTERNAL_GETMENU, 0, 0));
-						::DeleteMenu(menu2remove, lmi._cmdID, MF_BYCOMMAND);
+						::DeleteMenu(mainMenu, lmi._cmdID, MF_BYCOMMAND);
+
+						// If using compact language menu, walk through the first letter sub-menu
+						// and remove the sub-menu if it is empty after removing the language.
+						if (nppGUI._isLangMenuCompact)
+						{
+							HMENU subLangMenu = ::GetSubMenu(mainMenu, MENUINDEX_LANGUAGE);
+
+							int nbItem = ::GetMenuItemCount(subLangMenu);
+							int x = 0;
+							MENUITEMINFO menuItemInfo{};
+							menuItemInfo.cbSize = sizeof(MENUITEMINFO);
+							menuItemInfo.fMask = MIIM_FTYPE;
+							for (; x < nbItem; ++x)
+							{
+								::GetMenuItemInfo(subLangMenu, x, TRUE, &menuItemInfo);
+								if (menuItemInfo.fType & MFT_SEPARATOR)
+								{
+									break;
+								}
+							}
+
+							// Find the location in existing language menu to insert to. This includes submenu if using compact language menu.
+							wchar_t firstLetter = lmi._langName.empty() ? L'\0' : towupper(lmi._langName[0]);
+							wchar_t buffer[MAX_EXTERNAL_LEXER_NAME_LEN]{ L'\0' };
+							menuItemInfo.fMask = MIIM_SUBMENU;
+
+							for (++x; x < nbItem; ++x)
+							{
+								::GetMenuItemInfo(subLangMenu, x, TRUE, &menuItemInfo);
+								::GetMenuString(subLangMenu, x, buffer, MAX_EXTERNAL_LEXER_NAME_LEN, MF_BYPOSITION);
+
+								// Check if using compact language menu.
+								if (menuItemInfo.hSubMenu && buffer[0] == firstLetter)
+								{
+									// Found the sub-menu for the language's first letter. Search in it instead.
+									if (::GetMenuItemCount(menuItemInfo.hSubMenu) == 0)
+									{
+										::RemoveMenu(subLangMenu, x, MF_BYPOSITION);
+										::DestroyMenu(menuItemInfo.hSubMenu);
+										break;
+									}
+								}
+							}
+						}
 					}
-					else
+					else // IDC_BUTTON_RESTORE
 					{
-						auto menu = reinterpret_cast<HMENU>(::SendMessage(grandParent, NPPM_INTERNAL_GETMENU, 0, 0));
-						HMENU subMenu = ::GetSubMenu(menu, MENUINDEX_LANGUAGE);
+						HMENU subLangMenu = ::GetSubMenu(mainMenu, MENUINDEX_LANGUAGE);
 
 						// Find the first separator which is between IDM_LANG_TEXT and languages
-						int nbItem = ::GetMenuItemCount(subMenu);
+						int nbItem = ::GetMenuItemCount(subLangMenu);
 						int x = 0;
 						MENUITEMINFO menuItemInfo{};
 						menuItemInfo.cbSize = sizeof(MENUITEMINFO);
 						menuItemInfo.fMask = MIIM_FTYPE;
 						for (; x < nbItem; ++x)
 						{
-							::GetMenuItemInfo(subMenu, x, TRUE, &menuItemInfo);
+							::GetMenuItemInfo(subLangMenu, x, TRUE, &menuItemInfo);
 							if (menuItemInfo.fType & MFT_SEPARATOR)
 							{
 								break;
@@ -4847,18 +4890,21 @@ intptr_t CALLBACK LanguageSubDlg::run_dlgProc(UINT message, WPARAM wParam, LPARA
 						wchar_t firstLetter = lmi._langName.empty() ? L'\0' : towupper(lmi._langName[0]);
 						wchar_t buffer[MAX_EXTERNAL_LEXER_NAME_LEN]{ L'\0' };
 						menuItemInfo.fMask = MIIM_SUBMENU;
+
+						bool enteredLetterSubMenu = false;
+
 						for (++x; x < nbItem; ++x)
 						{
-							::GetMenuItemInfo(subMenu, x, TRUE, &menuItemInfo);
-							::GetMenuString(subMenu, x, buffer, MAX_EXTERNAL_LEXER_NAME_LEN, MF_BYPOSITION);
+							::GetMenuItemInfo(subLangMenu, x, TRUE, &menuItemInfo);
+							::GetMenuString(subLangMenu, x, buffer, MAX_EXTERNAL_LEXER_NAME_LEN, MF_BYPOSITION);
 
 							// Check if using compact language menu.
 							if (menuItemInfo.hSubMenu && buffer[0] == firstLetter)
 							{
 								// Found the submenu for the language's first letter. Search in it instead.
-								subMenu = menuItemInfo.hSubMenu;
-								nbItem = ::GetMenuItemCount(subMenu);
+								nbItem = ::GetMenuItemCount(menuItemInfo.hSubMenu);
 								x = -1;
+								enteredLetterSubMenu = true;
 							}
 							else if (lstrcmp(lmi._langName.c_str(), buffer) < 0)
 							{
@@ -4866,8 +4912,22 @@ intptr_t CALLBACK LanguageSubDlg::run_dlgProc(UINT message, WPARAM wParam, LPARA
 							}
 						}
 
-						::InsertMenu(subMenu, x, MF_BYPOSITION, lmi._cmdID, lmi._langName.c_str());
+						if (nppGUI._isLangMenuCompact && !enteredLetterSubMenu)
+						{
+							// The letter submenu was removed because it became empty; recreate it at the alphabetically correct position (x, computed above),
+							// then insert the language as its first (and only) entry.
+							HMENU newLetterSubMenu = ::CreatePopupMenu();
+							wchar_t letterLabel[2] = { firstLetter, L'\0' };
+							::InsertMenu(subLangMenu, x, MF_BYPOSITION | MF_POPUP,
+								reinterpret_cast<UINT_PTR>(newLetterSubMenu), letterLabel);
+							::InsertMenu(newLetterSubMenu, 0, MF_BYPOSITION, lmi._cmdID, lmi._langName.c_str());
+						}
+						else
+						{
+							::InsertMenu(subLangMenu, x, MF_BYPOSITION, lmi._cmdID, lmi._langName.c_str());
+						}
 					}
+
 					::DrawMenuBar(grandParent);
 					return TRUE;
 				}

PowerEditor/src/Notepad_plus.cpp
@@ -583,8 +583,23 @@ LRESULT Notepad_plus::init(HWND hwnd)
 			nppGUI._excludedLangList[i]._cmdID = cmdID;
 			nppGUI._excludedLangList[i]._langName = itemName;
 			::DeleteMenu(hLangMenu, cmdID, MF_BYCOMMAND);
-			DrawMenuBar(hwnd);
 		}
+
+		// If compact language menu is enabled, remove any submenus that became empty after exclusion
+		if (nppGUI._isLangMenuCompact)
+		{
+			int nbItems = ::GetMenuItemCount(hLangMenu);
+			for (int i = nbItems - 1; i >= 0; --i)
+			{
+				HMENU hSubMenu = ::GetSubMenu(hLangMenu, i);
+				if (hSubMenu && ::GetMenuItemCount(hSubMenu) == 0)
+				{
+					::RemoveMenu(hLangMenu, i, MF_BYPOSITION);
+				}
+			}
+		}
+
+		::DrawMenuBar(hwnd);
 	}
 
 	// Add User Defined Languages Entry

commit 693e4814eb98ca0a7134641386b5b393403e1eed

Fix Language menu restauration regression due to the previous commit
Regression was introduced by:
https://github.com/notepad-plus-plus/notepad-plus-plus/commit/c4ee1aff9ccc559ec94fdff0e7650a91277483ec

Fix:
https://github.com/notepad-plus-plus/notepad-plus-plus/commit/0e46d22e0f7df0e239e520de172c7d6fb4550d0f#commitcomment-193371952

PowerEditor/src/WinControls/Preference/preferenceDlg.cpp
@@ -4863,7 +4863,7 @@ intptr_t CALLBACK LanguageSubDlg::run_dlgProc(UINT message, WPARAM wParam, LPARA
 						menuItemInfo.fMask = MIIM_SUBMENU;
 
 						bool enteredLetterSubMenu = false;
-
+						HMENU subMenu2InsertIn = subLangMenu;
 						for (++x; x < nbLangItems; ++x)
 						{
 							::GetMenuItemInfo(subLangMenu, x, TRUE, &menuItemInfo);
@@ -4876,6 +4876,7 @@ intptr_t CALLBACK LanguageSubDlg::run_dlgProc(UINT message, WPARAM wParam, LPARA
 								nbLangItems = ::GetMenuItemCount(menuItemInfo.hSubMenu);
 								x = -1;
 								enteredLetterSubMenu = true;
+								subMenu2InsertIn = menuItemInfo.hSubMenu;
 							}
 							else if (lstrcmp(lmi._langName.c_str(), buffer) < 0)
 							{
@@ -4894,7 +4895,7 @@ intptr_t CALLBACK LanguageSubDlg::run_dlgProc(UINT message, WPARAM wParam, LPARA
 						}
 						else
 						{
-							::InsertMenu(subLangMenu, x, MF_BYPOSITION, lmi._cmdID, lmi._langName.c_str());
+							::InsertMenu(subMenu2InsertIn, x, MF_BYPOSITION, lmi._cmdID, lmi._langName.c_str());
 						}
 					}
 